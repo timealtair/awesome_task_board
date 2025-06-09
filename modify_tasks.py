2177cd09fab8a9031228a666c0bee9937ad3ec71
timealtair <83885014+timealtair@users.noreply.github.com>
2025-06-09 21:54:54 +0300

    added 'delete mechanic'

--- modify_tasks.py
+++ modify_tasks.py
@@ -1,15 +1,32 @@
 import sqlite3
+import sys
 from os import PathLike
 from utils.db_functions import (
-    get_table_from_user, get_status_from_user, get_items_by_status,
+    get_table_from_user, get_status_from_user, get_task_id_from_user,
+    get_modify_or_delete_selection, delete_task, modify_task,
 )
 from utils.prettify_functions import print_separator
 from settings.default_status_set import DEFAULT_STATUS_SET
 
 
-def modify_iter(table: str, conn: sqlite3.Connection):
+def modify_iter(table: str, conn: sqlite3.Connection, sep_symbol: str):
+    print_separator(sep_symbol)
     status = get_status_from_user(table, conn, DEFAULT_STATUS_SET)
-    print(get_items_by_status(table, status, conn))
+
+    print_separator(sep_symbol)
+    idx, tasks, schema = get_task_id_from_user(table, status, conn)
+    if idx is None:
+        return
+    mod_or_del = get_modify_or_delete_selection()
+    match mod_or_del:
+        case 0:
+            modify_task(...)
+        case 1:
+            delete_task(tasks, idx, table, conn)
+        case _:
+            raise RuntimeError(
+                'get_modify_or_delete_selection return not 0 1!'
+            )
 
 
 def main(db_file: str | PathLike, sep_symbol: str):
@@ -20,7 +37,7 @@ def main(db_file: str | PathLike, sep_symbol: str):
         table = get_table_from_user(conn)
 
         while True:
-            modify_iter(table, conn)
+            modify_iter(table, conn, sep_symbol)
     finally:
         conn.close()
 
