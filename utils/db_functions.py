29d81dec23507377db1bfbf5164d0e506a0ed7de
timealtair <83885014+timealtair@users.noreply.github.com>
2025-06-09 19:30:33 +0300

    added 'get_status_from_user' func

--- utils/db_functions.py
+++ utils/db_functions.py
@@ -1,4 +1,5 @@
 import sqlite3
+import logging
 
 
 def _get_and_parse_results(query: str, conn: sqlite3.Connection) -> list:
@@ -15,9 +16,40 @@ def get_existing_tables(conn: sqlite3.Connection) -> list:
     return _get_and_parse_results(query, conn)
 
 
-def get_existing_status_set(table_name: str, conn: sqlite3.Connection) -> set:
+def _get_existing_status_set(table_name: str, conn: sqlite3.Connection) -> set:
     query = f'select status from `{table_name}`'
     return set(_get_and_parse_results(query, conn))
+
+
+def get_status_from_user(
+    table: str, conn: sqlite3.Connection, default_status_set: dict,
+) -> str:
+    existed_status_set = _get_existing_status_set(table, conn)
+    default_status_set.update(
+        dict.fromkeys(existed_status_set)
+    )
+    status_list = list(default_status_set)
+    print('Available status numbers:')
+    for num, status in enumerate(status_list):
+        print(f'{num}. {status}')
+    custom_num = len(status_list)
+    print(f'{custom_num}. custom')
+    while True:
+        choice = input('Enter your status number (0 is default): ')
+        if not choice.strip():
+            choice = 0
+        try:
+            choice = int(choice)
+        except ValueError:
+            logging.error('Status should be valid number!')
+            continue
+        try:
+            return status_list[choice]
+        except IndexError:
+            if choice == custom_num:
+                return input('Enter custom status: ')
+            else:
+                logging.error('Status number is out of range!')
 
 
 if __name__ == '__main__':
@@ -25,9 +57,15 @@ if __name__ == '__main__':
 
     db_file = r'./database/todo.db'
 
+    DEFAULT_STATUS_SET = dict.fromkeys((
+        'todo',
+        'in progress',
+        'done',
+    ))
+
     conn = sqlite3.Connection(db_file)
     print('Found tables:')
     print(get_existing_tables(conn))
     table = input('Enter table name: ')
-    print('Found statuses:')
-    print(get_existing_status_set(table, conn))
+    status = get_status_from_user(table, conn, DEFAULT_STATUS_SET)
+    print('You selected status:', status)
